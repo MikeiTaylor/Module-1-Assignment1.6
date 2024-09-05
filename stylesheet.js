477124be4000f021e33113ec22970152bfe80f13
MikeiTaylor <143642773+MikeiTaylor@users.noreply.github.com>
2024-09-05 13:40:15 -0500

    Block and Global Scopes

--- stylesheet.js
+++ stylesheet.js
@@ -1,20 +1,20 @@
 // JavaScript source code
 
-var life= 100; //This is my life bar
+var life= 100; //This is my life bar, Global
 life=life - 10;
 life=life / 10;
-var name="Michael"; //This is the STRING
-var checkout= false; //BOOLEAN
-var box = null; //
+var name="Michael"; //This is the STRING, Global
+var checkout= false; //BOOLEAN, Global
+var box = null; // Global
 
-const Hp = 1000;
+const Hp = 1000; //Global
 
 
-let value=100;
+{let value=200;} //Block
 value=value - 50;
 console.log("How can we use console log on a webpage?");
 console.log(life);
 console.log(box);
 console.log(name);
-console.log(checkout)
-console.log(Hp-value =)
+console.log(checkout);
+console.log(Hp-200=);
