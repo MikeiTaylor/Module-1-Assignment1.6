c3d0150791ec16fe2cb948e0fb329de38d675ee7
MikeiTaylor <143642773+MikeiTaylor@users.noreply.github.com>
2024-09-05 13:27:06 -0500

    Attempt to add var

--- stylesheet.js
+++ stylesheet.js
@@ -1,2 +1,20 @@
 // JavaScript source code
-console.log(Hey Professor I was wondering why we would need to use the console log?);
+
+var life= 100; //This is my life bar
+life=life - 10;
+life=life / 10;
+var name="Michael"; //This is the STRING
+var checkout= false; //BOOLEAN
+var box = null; //
+
+const Hp = 1000;
+
+
+let value=100;
+value=value - 50;
+console.log("How can we use console log on a webpage?");
+console.log(life);
+console.log(box);
+console.log(name);
+console.log(checkout)
+console.log(Hp-value =)
